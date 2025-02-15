307e0933f44898a68ffef222df5e77c0c9bc7062
jmax22 <maxwelljentry33@gmail.com>
2025-02-14 20:31:47 -0700

    Button functionality now working with switching screens.

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
-  
+
   @override
-  State<Quiz> createState() => _QuizState();  
+  State<Quiz> createState() => _QuizState();
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -24,10 +39,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: screenWidget,
         ),
       ),
     );
   }
-
 }
