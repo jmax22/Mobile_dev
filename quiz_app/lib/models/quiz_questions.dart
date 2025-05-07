b619eda6401ecce43f4d189f40aeacade4c9caf4
jmax22 <maxwelljentry33@gmail.com>
2025-05-07 15:25:17 -0700

    Added new functionality to buttons/custom widget to style all the buttons to one format

---
+++ quiz_app/lib/models/quiz_questions.dart
@@ -0,0 +1,8 @@
+class QuizQuestions {
+
+  const QuizQuestions(this.text, this.answers);
+  final String text;
+  final List<String> answers;
+
+
+}
--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,7 +12,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Some text goes here!');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text('The Questions...', style: TextStyle(
+          color: Colors.white,
+        ),), 
+        const SizedBox(height: 30),
+        AnswerButton(answerText: 'Answer 1', onTap: () {}
+        ),
+        AnswerButton(answerText: 'Answer 2', onTap: () {}
+        ),
+        AnswerButton(answerText: 'Answer 3', onTap: () {}
+        ),
+        AnswerButton(answerText: 'Answer 4', onTap: () {}
+        ),
+      ],
+      ),
+    );
   }
 
 }
