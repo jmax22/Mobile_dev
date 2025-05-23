9396b6c568d9d390e309899aa5573f3db512a463
jmax22 <maxwelljentry33@gmail.com>
2025-05-22 21:43:40 -0700

    Rendering Long Lists with ListView builder widget / implementing Lists inside of Lists

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,25 +1,42 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
-
   const Expenses({super.key});
 
   @override
   State<StatefulWidget> createState() {
     return _ExpensesState();
-  }  
-  
+  }
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Dummy Text',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Dummy Text2',
+      amount: 4.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('The Chart'),
-          Text('Expenses List...')
-        ],
+        children: [
+          const Text('The Chart'), 
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+          ],
       ),
     );
   }
